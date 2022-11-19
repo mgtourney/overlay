16a006dc64489901ca3c060b8de6d2b3ab3cf07e
Samuel Braun <office.samigo.a@gmail.com>
2022-11-19 15:01:00 +0100

    feat(modding): :sparkles: Added mod communication to overlays

--- src/components/ModView.vue
+++ src/components/ModView.vue
@@ -3,6 +3,19 @@ import Login from './Login.vue';
 import { useAuth0 } from "@auth0/auth0-vue";
 const { user, isAuthenticated } = useAuth0();
 
+const relaySocket = new WebSocket("ws://localhost:2223");
+relaySocket.onopen = () => {
+    console.log("Relay connected");
+};
+
+function updateLead() {
+    relaySocket.send(JSON.stringify({
+        command: "update_lead",
+        leftLead: 0,
+        rightLead: 1,
+    }));
+}
+
 /**
  * For some reason, the url hash gets lost when the user is redirected back to the app after logging in.
  */
@@ -14,7 +27,7 @@ setTimeout(() => {
 <template>
     <div v-if="isAuthenticated">
         <h1>Logged in as {{ user.name }}</h1>
-        
+        <button @click="updateLead">ghjgg</button>
 
     </div>
     <Login v-else />
